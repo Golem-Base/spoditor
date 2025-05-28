3168b99c6d84bbe7353c2b75e63a92f69e32e1b2
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2025-05-28 13:38:02 +0200

    feat: improve comments in ss_identifier

--- internal/ss_identifier.go
+++ internal/ss_identifier.go
@@ -12,18 +12,26 @@ import (
 
 var podIdentifierLog = logf.Log.WithName("pod_identifier")
 
+// SSPodIdentifier defines the interface for extracting StatefulSet information from pods
 type SSPodIdentifier interface {
+	// Extract returns the StatefulSet name and pod ordinal from the object metadata
+	// Returns an error if the pod is not part of a StatefulSet or if information is missing
 	Extract(accessor v1.ObjectMetaAccessor) (string, int, error)
 }
 
+// SSPodIdentifierFunc is a function type that implements SSPodIdentifier
 type SSPodIdentifierFunc func(v1.ObjectMetaAccessor) (string, int, error)
 
+// Extract implements the SSPodIdentifier interface for SSPodIdentifierFunc
 func (f SSPodIdentifierFunc) Extract(accessor v1.ObjectMetaAccessor) (string, int, error) {
 	return f(accessor)
 }
 
 var _ SSPodIdentifier = SSPodIdentifierFunc(nil)
 
+// LabelSSPodIdentifier extracts StatefulSet information from pod labels
+// It looks for the standard Kubernetes label "statefulset.kubernetes.io/pod-name"
+// which follows the format "<statefulset-name>-<ordinal>"
 var LabelSSPodIdentifier SSPodIdentifierFunc = func(accessor v1.ObjectMetaAccessor) (string, int, error) {
 	l, ok := accessor.GetObjectMeta().GetLabels()["statefulset.kubernetes.io/pod-name"]
 	if !ok {
@@ -31,9 +39,13 @@ var LabelSSPodIdentifier SSPodIdentifierFunc = func(accessor v1.ObjectMetaAccess
 		return "", -1, errors.New("missing statefulset label")
 	}
 	podIdentifierLog.Info("stateful pod name", "name", l)
+
+	// Validate label format: must be "<name>-<number>"
 	if b, err := regexp.MatchString(".+-\\d+", l); err != nil || !b {
 		return "", -1, errors.New("unexpected label value")
 	}
+
+	// Split the pod name to extract the StatefulSet name and ordinal
 	i := strings.LastIndex(l, "-")
 	ordinal, _ := strconv.Atoi(l[i+1:])
 	return l[:i], ordinal, nil
